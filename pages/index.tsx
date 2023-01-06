862599a00866b073548779a11aa5bb7840acf7a9
Maria <lorincz_maria@yahoo.com>
2023-01-06 16:30:26 +0200

    added new features

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,23 @@ import Head from 'next/head'
 import type { NextPage } from 'next'
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
 
 const Home : NextPage = () => {
 
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Maria's Portfolio</title>
       </Head>
       <Header/>
        {/* Hero */}
-       <section id="hero">
+       <section id="hero" className='snap-start'>
        <Hero/>
        </section>
        {/* About */}
+       <section id='about' className='snap-center'> <About/> </section>
        {/* Experience */}
        {/* Skills*/}
        {/* Projects */}
