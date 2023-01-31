d04450ec721b72adfcd0af861424be409c60ba9b
mcrzx <lorincz_maria@yahoo.com>
2023-01-31 13:07:50 +0200

    expiernce part

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
+import WorkExperience from '../components/WorkExperience';
 
 
 const Home : NextPage = () => {
@@ -17,9 +18,11 @@ const Home : NextPage = () => {
        <section id="hero" className='snap-start'>
        <Hero/>
        </section>
-       {/* About */}
        <section id='about' className='snap-center'> <About/> </section>
        {/* Experience */}
+       <section id='experience' className='snap-center'>
+        <WorkExperience/>
+       </section>
        {/* Skills*/}
        {/* Projects */}
        {/* Contact Me*/}
---
+++ portfolio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'uw7wvjf5',
+    dataset: 'production'
+  }
+})
